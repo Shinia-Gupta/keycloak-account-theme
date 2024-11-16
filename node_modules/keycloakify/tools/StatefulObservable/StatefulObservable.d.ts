167506965fcb6153ec65bdf6b17c52e699be2150
Shinia <113818197+Shinia-Gupta@users.noreply.github.com>
2024-11-16 10:16:23 +0530

    Delete node_modules directory

--- node_modules/keycloakify/tools/StatefulObservable/StatefulObservable.d.ts
+++
@@ -1,8 +0,0 @@
-export type StatefulObservable<T> = {
-    current: T;
-    subscribe: (next: (data: T) => void) => Subscription;
-};
-export type Subscription = {
-    unsubscribe(): void;
-};
-export declare function createStatefulObservable<T>(getInitialValue: () => T): StatefulObservable<T>;
--- node_modules/keycloakify/tools/deepAssign.d.ts
+++
@@ -1,5 +0,0 @@
-/** NOTE: Array a copied over, not merged. */
-export declare function deepAssign(params: {
-    target: Record<string, unknown>;
-    source: Record<string, unknown>;
-}): void;
